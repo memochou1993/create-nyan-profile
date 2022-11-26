5dc5263ca370b8e5992f43e33253aab35c084344
Memo Chou <memochou1993@gmail.com>
2022-11-26 22:39:25 +0800

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -4,14 +4,6 @@ git clone git@github.com:memochou1993/nyan-profile.git
 
 cd nyan-profile
 
-if [[ $1 != "" ]]; then
-    sed -i "" "2s/nyan-profile/$1/" "package.json"
-    sed -i "" "2s/nyan-profile/$1/" "package-lock.json"
-    sed -i "" "8s/nyan-profile/$1/" "package-lock.json"
-    sed -i "" "5s/nyan-profile/$1/" "docker-compose.yaml"
-    sed -i "" "2s/nyan-profile/$1/" "nyan.config.json"
-fi
-
 rm -rf .git
 
 git init
